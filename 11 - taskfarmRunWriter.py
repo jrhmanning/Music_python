4ee0c8cf425788213a5972bfe580e3821a84efa9
jrhmanning <jrhm21@bath.ac.uk>
2019-08-29 13:34:50 +0100

    switch to setup module functions - untested!

--- 11 - taskfarmRunWriter.py
+++ 11 - taskfarmRunWriter.py
@@ -5,10 +5,18 @@
 #parentdir = a path to navigate you to your testing directory
 #################
 import os
+import re
+import random
+import numpy as np
 from math import sqrt, log
-import logging
 from pathlib import Path
+import logging
 import datetime
+import argparse
+
+import musicpy.setup as setup
+import musicpy.Antoine as Antoine
+import musicpy.Forcefield as Forcefield
 now = datetime.datetime.now()
 #################
 ####This section sorts out your messages from this script to the console and a log file
@@ -28,41 +36,6 @@ species = ['XXXX']
 T = 298
 framework = 'YYYY'
 parentdir = Path('/home/r/jrhm21/scratch/03_music_chloroform_forcefield/')
-##################
-#Writes a bashscript for taskfarming yuor simulations, compatible with SLURM
-def TaskfarmRunWriter(Species, T, framework, parentdir, dirout):
-    with open("{0}run.taskfarmer" .format(dirout), 'w') as file:
-        file.write("""#!/usr/bin/env bash
-#SBATCH --job-name={0}.{2}.{1}k
-#SBATCH --partition=batch
-#SBATCH --nodes=1
-#SBATCH --tasks-per-node=16
-#SBATCH --time=06:00:00
-#SBATCH --mail-type=END
-#SBATCH --mail-user=jrhm21@bath.ac.uk #Change this to your email!
-#SBATCH account=free
-
-# -- Set up the environment
-module purge
-module load group ce-molsim stack
-module load taskfarmer
-
-#locate the experiment directory
-cd {4} #Change this bit to get to your directory
-cd {3}
-
-# -- Run
-mpirun -np 16 taskfarmer -f taskfarm
-
-# -- python analyse the results
-python ./isothermextractor.py""".format(Species, T, framework, dirout, parentdir)) #uses my isotherm extractor script to pull all your isotherms out and put them into .csv files in the directory abocve the taskfarm run file
-    os.chmod("{0}run.taskfarmer" .format(dirout), 0o777)   #chmod so it's fully rwx for everyone (0o denotes the following number is in octal)
-    with open("{0}taskfarm" .format(dirout), 'w') as file1: #Writes the commands to taskfarm into a separate taskfram file
-        for i in range(1,17):
-            file1.write("bash ./{0:02d}/run.gcmc\n".format(i))
-    with open("{0}taskfarm.backup" .format(dirout), 'w') as file2: #writes a backup you can restore the above to if there's a bug
-        for i in range(1,17):
-            file2.write("bash ./{0:02d}/run.gcmc\n".format(i))
-    logger.debug("Taskfarm things file written!")
+targetfir= Path('./experiments/{0}/{1}/'.format(species[-1], T))
 ############
-TaskfarmRunWriter(species[-1], T, framework, parentdir, './experiments/{0}/{1}/'.format(species[-1], T))
+setup.TaskfarmRunWriter(species[-1], T, framework, parentdir, targetdir)
