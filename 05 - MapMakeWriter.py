7b2c992cd486355143fcdee715aac1ed9c2280bc
jrhmanning <jrhm21@bath.ac.uk>
2019-08-29 13:15:22 +0100

    switch to setup functions - untested!

--- 05 - MapMakeWriter.py
+++ 05 - MapMakeWriter.py
@@ -6,10 +6,18 @@
 #parentdir = a path to navigate you to your testing directory
 #################
 import os
+import re
+import random
+import numpy as np
 from math import sqrt, log
-import logging
 from pathlib import Path
+import logging
 import datetime
+import argparse
+
+import musicpy.setup as setup
+import musicpy.Antoine as Antoine
+import musicpy.Forcefield as Forcefield
 now = datetime.datetime.now()
 #################
 ####This section sorts out your messages from this script to the console and a log file
@@ -29,35 +37,7 @@ species = ['XXXX']
 framework = 'YYYY'
 sorb_el_list = ['''### your species list here###''']
 parentdir = Path('/home/r/jrhm21/scratch/03_music_chloroform_forcefield/')
-#################
-#Writes a bashscript for making your maps, compatible with SLURM
-def MapMakeRunWriter(species, elements, parentdir, dxout = "./mapgen/"):
-    with open("{0}run.mapmaker" .format(dxout), "w") as file:
-        file.write("""#!/usr/bin/env bash 
-#SBATCH --job-name={0}.map
-#SBATCH --partition=batch
-#SBATCH --nodes=1
-#SBATCH --tasks-per-node=1
-#SBATCH --time=06:00:00
-#SBATCH --mail-type=END
-#SBATCH --mail-user=jrhm21@bath.ac.uk #Change this to your email!
-#SBATCH account=free
-
-# -- Set up the environment
-module purge
-module load group ce-molsim stack
-module load music/std
-
-# -- Find my directory
-cd {2}{1}\n #Change this bit to get to your directory
-export ATOMSDIR=../../atoms
-export MOLSDIR=../../molecules
-export PMAPDIR=../../maps/{0}
-export EMAPDIR=../../maps
-\n\n# --Run\n""" .format(species, dxout.split(".")[-1], parentdir))
-        for count, i in enumerate(elements, 1):
-            file.write("music_mapmaker makemap_{0}.ctr > logfile{1}\n" .format(i, count, parentdir))
-    os.chmod("{0}run.mapmaker" .format(dxout), 0o777) #chmod so it's fully rwx for everyone (0o denotes the following number is in octal)
-    logger.debug("Runfile written!")   
+targetdir = Path("./mapgen/{0}".format(species[-1]))
+################
 ###########
-MapMakeRunWriter(species[-1], sorb_el_list, '{0}/'.format(parentdir), "./mapgen/{0}".format(species[-1]))
+setup.MapMakeRunWriter(logger, species[-1], sorb_el_list, '{0}/'.format(parentdir), targetdir)
