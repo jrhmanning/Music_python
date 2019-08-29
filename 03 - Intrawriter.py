f7d36e92f27b05d2b796bfa5986963c82dd42f4d
jrhmanning <jrhm21@bath.ac.uk>
2019-08-29 13:10:40 +0100

    switch to setup module - untested!

--- 03 - Intrawriter.py
+++ 03 - Intrawriter.py
@@ -4,10 +4,19 @@
 #elements - a list of the elemetns involved in your sorbent species, for maps
 #framework - the name of your MOF (or other porous material
 #################
+import os
+import re
+import random
+import numpy as np
 from math import sqrt, log
-import logging
 from pathlib import Path
+import logging
 import datetime
+import argparse
+
+import musicpy.setup as setup
+import musicpy.Antoine as Antoine
+import musicpy.Forcefield as Forcefield
 now = datetime.datetime.now()
 #################
 ####This section sorts out your messages from this script to the console and a log file
@@ -23,18 +32,10 @@ logger.debug('-----------------------------------')
 logger.debug('Generating new files on {0}-{1}-{2} at {3}:{4}'.format(now.year, now.month, now.day, now.hour, now.minute))
 logger.debug('-----------------------------------')
 ##################
+####This section sorts out your list of species' and frameworks to amke an intramolecular file for you
 species = ['XXXX']
 framework = 'YYYY'
-sorb_el_list = ['''### your species list here###''']
-#################
-#Writes your intramolecular file
-def IntraWriter(species, elements, framework, dxout = "./mapgen/"):
-    with open("{0}intramolecular_file" .format(dxout), "w") as file:
-        for i in species:
-            file.write("Intra: {0}\n" .format(i)) #no intramolecular interactions at all
-        for i in list(elements): #lets you create a map of every element in your system. Having the extra lines in your production script don't matter
-            file.write("Intra: {0}\n".format(i))
-        file.write("Intra: {0}" .format(framework)) #No framework intramolecular interactions happening
-    logger.debug("Intra file written!")
+sorb_el_list = ['''### your element list here###''']
+targetdirectory='./experiments/{0}/298/'.format(species)
 ##################
-ntraWriter(species, sorb_el_list, framework, './experiments/{0}/298/'.format(species))
+setup.IntraWriter(logger, species, sorb_el_list, framework, targetdirectory)
