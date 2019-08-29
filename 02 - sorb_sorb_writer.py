f38176b5d1fd170a855b370f89601170db6687b1
jrhmanning <jrhm21@bath.ac.uk>
2019-08-29 13:35:52 +0100

    switch to setup functions - untested!

--- 02 - sorb_sorb_writer.py
+++ 02 - sorb_sorb_writer.py
@@ -4,10 +4,19 @@
 #elements - a list of the elemetns involved in your sorbent species, for maps
 #framework - the name of your MOF (or other porous material
 #################
+import os
+import re
+import random
+import numpy as np
 from math import sqrt, log
-import logging
 from pathlib import Path
+import logging
 import datetime
+import argparse
+
+import musicpy.setup as setup
+import musicpy.Antoine as Antoine
+import musicpy.Forcefield as Forcefield
 now = datetime.datetime.now()
 #################
 ####This section sorts out your messages from this script to the console and a log file
@@ -26,34 +35,6 @@ logger.debug('-----------------------------------')
 species = ['XXXX']
 framework = 'YYYY'
 sorb_el_list = ['''### your species list here###''']
-
-##################
-#This writes your sorb-sorb file telling music which intermolecular interactions are on
-def SorbSorbWriter(species, elements,framework, dxout = "./mapgen/", pmap = True, emap = False):
-    filename = "sorb_sorb_file"
-    with open("{0}{1}" .format(dxout, filename), 'w') as file:
-        file.write("{0} {0} NCOUL OFF\n{0} {0} COUL OFF\n\n" .format(framework)) #MOF-MOF interactions are off
-        if pmap == True: #using a MOF-fluid LJ potential map if true
-            for i in species:            
-                file.write("{0} {1} NCOUL MAP@{1} FAST ".format(i, framework)) #declares it'll use a map
-                for j in elements:
-                    file.write("{0}@PMAP@{1}.{0}.p " .format(j, framework)) #writes your map names. WARNING - it's a 18 character limit on map names, and no '-' allowed!
-                file.write("\n")
-        else:
-            for i in species:
-                file.write("{0} {1} NCOUL BASIC LJ FAST\n".format(i, framework)) #pairwise fluid-framework interactions
-        if emap == True: #using a MOF-fluid coulomb map if true
-            for i in species:
-                file.write("{0} {1} COUL MAP@{1} FAST ".format(i, framework)) #declares it'll use a map
-                for j in elements:
-                    file.write("{0}@EMAP@{1}.{2}.e " .format(j, framework, 'Probe')) #writes your map names. WARNING - it's a 18 character limit on map names, and no '-' allowed!
-                file.write("\n")
-        else:
-            for i in species:
-                file.write("\n{0} {1} COUL OFF\n" .format(i, framework)) #pairwise coulomb interactionsdon't work. Trust me, this is better.
-            logger.warning("Framework-fluid coulombic interactions are off") #warns you your coulomb fluid-framework stuff isn't happeneing
-        for i in species:
-            file.write("\n{0} {0} NCOUL BASIC LJ FAST\n{0} {0} COUL BASIC WFCOUL FAST\n\n" .format(i))  #fluid fluid interactions, with Wolf coulombic interactions 
-    logger.debug("Sorb-Sorb file written!")    
+targetdir = Path('./experiments/{0}/298/'.format(species))
 #########
-SorbSorbwriter(species, elements, framework, './experiments/{0}/298/'.format(species), True, True)
+setup.SorbSorbwriter(logger, species, elements, framework, targetdir, True, True)
